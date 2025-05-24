0d99516295375c69553ae447b68f090d66c3a24f
FelixJacinto4 <florentin.tosetti@ecole.ensicaen.fr>
2025-05-24 19:12:10 +0200

    Update ListProjet.h

--- GrandPrix2024_4.0.4/laCaenrosserie/ListProjet.h
+++ GrandPrix2024_4.0.4/laCaenrosserie/ListProjet.h
@@ -1,15 +1,19 @@
 #ifndef LIST_H
 #define LIST_H
 
-typedef struct Node{
-    int x;
-    int y;
+typedef struct Node {
+    int x, y;
     int direction;
     double heuristic;
     double total_cost;
+    double g, f;
+    int id;
+    struct Node* voisins[MAX_NEIGHBORS];
+    int nbVoisins;
     struct Node *nextCell;
     struct Node *parent;
 } Node, *List;
+
 
 List newList();
 
